1cf95f19154bce1295b9260f7a6cb01f80b16531
yingjieweb <1184061078@qq.com>
2020-10-29 22:22:00 +0800

    encapsulate basic Nav and Body layout comp

---
+++ src/views/SunBody.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="sun-body">
+    这就是个body
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "SunBody"
+  }
+</script>
+
+<style lang="scss" scoped>
+  .sun-body {
+    height: 100%;
+    background-color: red;
+  }
+</style>
---
+++ src/views/SunNav.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="sun-nav">
+    <div class="sun-item"></div>
+    <div class="sun-item"></div>
+    <div class="sun-item"></div>
+    <div class="sun-item"></div>
+    <div class="sun-item"></div>
+    <div class="sun-item"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "SunNav"
+  }
+</script>
+
+<style lang="scss" scoped>
+  .sun-nav {
+    height: 100%;
+    width: 80%;
+    background-color: darkkhaki;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    .sun-item {
+      height: 100px;
+      width: 100px;
+      background-color: seagreen;
+      margin: 10px;
+    }
+  }
+</style>
