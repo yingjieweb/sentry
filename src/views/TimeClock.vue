41e46db507590729afe909d2733d34cbe031c1ae
yingjieweb <1184061078@qq.com>
2020-11-04 16:30:16 +0800

    implement TimeClock styles and function

--- src/views/TimeClock.vue
+++ src/views/TimeClock.vue
@@ -1,13 +1,124 @@
 <template>
-  <div>TimeClock</div>
+  <div class="container">
+    <div class="clock">
+      <div class="hour" id="hour">
+        <!--<div class="hr"></div>-->
+      </div>
+      <div class="minute" id="minute">
+        <!--<div class="mn"></div>-->
+      </div>
+      <div class="second" id="second">
+        <!--<div class="sc"></div>-->
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="js">
   export default {
-    name: "TimeClock"
+    name: "TimeClock",
+    mounted() {
+      this.getCurrentTime()
+    },
+    methods: {
+      getCurrentTime() {
+        let hour = document.getElementById('hour');
+        let minute = document.getElementById('minute');
+        let second = document.getElementById('second');
+
+        setInterval(() => {
+          let day = new Date()
+          let hh = day.getHours() * 30
+          let mn = day.getMinutes() * 6
+          let sc = day.getSeconds() * 6
+          hour.style.transform = `rotateZ(${(hh) + (mn/12)}deg)`
+          minute.style.transform = `rotateZ(${mn}deg)`
+          second.style.transform = `rotateZ(${sc}deg)`
+        }, 1000)
+      }
+    }
   }
 </script>
 
 <style lang="scss" scoped>
+  .container {
+    background: #091921;
+  }
 
+  .clock {
+    width: 350px;
+    height: 350px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: url("../assets/images/clock/clock.png");
+    background-size: cover;
+    border: 4px solid #091921;
+    border-radius: 50%;
+    box-shadow: 0 -15px 15px rgba(255, 255, 255, 0.05),
+          inset 0 -15px 15px rgba(255, 255, 255, 0.05),
+                0 15px 15px rgba(0, 0, 0, 0.3),
+          inset 0 15px 15px rgba(0, 0, 0, 0.3);
+  }
+
+  /* 钟的内轴 */
+  .clock:before {
+    content: '';
+    position: absolute;
+    width: 15px;
+    height: 15px;
+    background: #fff;
+    border-radius: 50%;
+    z-index: 10;
+  }
+
+  .clock .hour,
+  .clock .minute,
+  .clock .second {
+    position: absolute;
+    display: flex;
+    justify-content: center;
+  }
+
+  .clock .hour {
+    width: 160px;
+    height: 160px;
+  }
+  .clock .minute {
+    width: 190px;
+    height: 190px;
+  }
+  .clock .second {
+    width: 230px;
+    height: 230px;
+  }
+
+  /* 时针样式 */
+  .clock .hour:before {
+    content: '';
+    position: absolute;
+    width: 8px;
+    height: 80px;
+    background-color: #ff105e;
+    border-radius: 6px 6px 0 0;
+    z-index: 2;
+  }
+  .clock .minute:before {
+    content: '';
+    position: absolute;
+    width: 4px;
+    height: 90px;
+    background-color: #ffffff;
+    border-radius: 6px 6px 0 0;
+    z-index: 3;
+  }
+  .clock .second:before {
+    content: '';
+    position: absolute;
+    width: 2px;
+    height: 150px;
+    background-color: #ffffff;
+    border-radius: 6px 6px 0 0;
+    z-index: 4;
+  }
 </style>
