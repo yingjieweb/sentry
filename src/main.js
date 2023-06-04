5d541c4f92ece71c037faaac3a5984b7e5bedfff
yingjieweb <yingjieweb@qq.com>
2023-06-04 11:39:03 +0800

    feat: 🎸 delete router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
 
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
@@ -10,6 +9,5 @@ Vue.config.productionTip = false
 Vue.use(ElementUI);
 
 new Vue({
-  router,
   render: h => h(App)
 }).$mount('#app')
